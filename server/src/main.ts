8aef543ddabbc23d5fd9f7ac180f06cdb116e2ba
gwonyejun <kyjuni1059@gmail.com>
2023-07-16 17:15:02 +0900

    kakao login api 연구해 보기 Fixes #1 page 폴더 생성

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import * as bodyParser from 'body-parser';
+import * as express from 'express';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useLogger(new Logger('debug'));
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use(express.static(join(__dirname, '..', 'client', 'build')));
+
   await app.listen(3000);
 }
+
 bootstrap();
