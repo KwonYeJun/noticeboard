d5bd7d6251cff09beca285a1db8acd554715ae58
gwonyejun <kyjuni1059@gmail.com>
2023-07-16 17:16:58 +0900

    페이지 요청을 받으면 응답 해줄수 있도록 작성

--- server/src/page/page.controller.ts
+++ server/src/page/page.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Res, Req } from '@nestjs/common';
 
-@Controller('page')
-export class PageController {}
+import { Response, Request } from 'express';
+import { join } from 'path';
+@Controller()
+export class PageController {
+  @Get('*')
+  serveFile(@Req() req: Request, @Res() res: Response) {
+    const filePath = join(__dirname, '..','..', 'client', 'build', 'index.html');
+    return res.sendFile(filePath);
+  }
+
+}
