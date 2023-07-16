ccd726e62e20ccd10696d1348be6e0826155536e
gwonyejun <kyjuni1059@gmail.com>
2023-07-16 16:03:41 +0900

    테스트 콘솔 지우기

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,7 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  console.log('dlrjs app 이야')
+
   return (
 <></>
   );
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -7,11 +7,9 @@ import App from './App'
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-console.log('index 이야')
 
 root.render(
   <>
-    <div>test</div>
     <App />
   </>
 );
