edba7463e467cf6597156f29a57254f055d465e6
gwonyejun <kyjuni1059@gmail.com>
2023-07-16 17:33:20 +0900

    갱신된 파일들 커밋

--- server/dist/page/page.module.js
+++ server/dist/page/page.module.js
@@ -5,20 +5,17 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-var __metadata = (this && this.__metadata) || function (k, v) {
-    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.AppController = void 0;
+exports.PageModule = void 0;
 const common_1 = require("@nestjs/common");
-const app_service_1 = require("./app.service");
-let AppController = exports.AppController = class AppController {
-    constructor(appService) {
-        this.appService = appService;
-    }
+const page_controller_1 = require("./page.controller");
+const page_service_1 = require("./page.service");
+let PageModule = exports.PageModule = class PageModule {
 };
-exports.AppController = AppController = __decorate([
-    (0, common_1.Controller)(),
-    __metadata("design:paramtypes", [app_service_1.AppService])
-], AppController);
-//# sourceMappingURL=app.controller.js.map
+exports.PageModule = PageModule = __decorate([
+    (0, common_1.Module)({
+        controllers: [page_controller_1.PageController],
+        providers: [page_service_1.PageService]
+    })
+], PageModule);
+//# sourceMappingURL=page.module.js.map
