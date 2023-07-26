73045bfa63e3cbd00e95399b97396c70c7f3f8ef
gwonyejun <kyjuni1059@gmail.com>
2023-07-26 16:16:12 +0900

    useSWR해결 버전

--- client/src/components/KakaoLogin.tsx
+++ client/src/components/KakaoLogin.tsx
@@ -4,36 +4,14 @@ import axios from 'axios';
 import useSWR from 'swr';
 
 const KakaoLoginComponent: React.FC = () => {
-  const kakaoClientId = 'YOUR_KAKAO_JAVASCRIPT_KEY';
+  const kakaoClientId = 'b9378165b8dd0245624ae217492f4ea6';
 
-  const kakaoOnSuccess = async (data: any) => {
-    try {
-      console.log(data);
-      const idToken = data.response.access_token; // 엑세스 토큰 백엔드로 전달
-
-      // SWR을 이용하여 요청 결과를 상태 관리
-      const { data: responseData, error } = useSWR(`/api/some-endpoint?accessToken=${idToken}`, (url) => axios.get(url).then((res) => res.data));
-      
-      if (responseData) {
-        // 요청이 성공했을 때 처리할 로직
-        console.log('Response:', responseData);
-      }
-
-      if (error) {
-        // 요청이 실패했을 때 처리할 로직
-        console.log('Error:', error);
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  const fetchUserData = async (url: string) => {
+    const response = await axios.get(url);
+    return response.data;
   };
 
-  const kakaoOnFailure = (error: any) => {
-    console.log(error);
-  };
-
-  // 이 부분에서 SWR을 사용하여 요청을 보내도록 수정
-  const { data: responseData, error } = useSWR(`/api/some-endpoint`, (url) => axios.get(url).then((res) => res.data));
+  const { data: responseData, error } = useSWR(`/api/some-endpoint?accessToken=${kakaoClientId}`, fetchUserData);
 
   useEffect(() => {
     if (responseData) {
@@ -46,6 +24,23 @@ const KakaoLoginComponent: React.FC = () => {
       console.log('Error:', error);
     }
   }, [responseData, error]);
+
+  const kakaoOnSuccess = async (data: any) => {
+    try {
+      console.log(data);
+      const idToken = data.response.access_token; // 엑세스 토큰 백엔드로 전달
+
+      // 요청 보내기
+      const response = await axios.get(`/api/some-endpoint?accessToken=${idToken}`);
+      console.log('Response:', response.data);
+    } catch (error) {
+      console.log('Error:', error);
+    }
+  };
+
+  const kakaoOnFailure = (error: any) => {
+    console.log(error);
+  };
 
   return (
     <>
