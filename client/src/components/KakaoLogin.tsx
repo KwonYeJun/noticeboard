0a8e9279a5846c2aa92c2137c4cbe6951b8b63ef
gwonyejun <kyjuni1059@gmail.com>
2023-07-26 16:13:04 +0900

    useSWR에러가 발생 하였다.

--- client/src/components/KakaoLogin.tsx
+++ client/src/components/KakaoLogin.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import KakaoLogin from "react-kakao-login";
 import axios from 'axios';
 import useSWR from 'swr';
@@ -11,9 +11,6 @@ const KakaoLoginComponent: React.FC = () => {
       console.log(data);
       const idToken = data.response.access_token; // 엑세스 토큰 백엔드로 전달
 
-      // 요청 보내기
-      const response = await axios.get(`/api/some-endpoint?accessToken=${idToken}`);
-      
       // SWR을 이용하여 요청 결과를 상태 관리
       const { data: responseData, error } = useSWR(`/api/some-endpoint?accessToken=${idToken}`, (url) => axios.get(url).then((res) => res.data));
       
@@ -34,6 +31,21 @@ const KakaoLoginComponent: React.FC = () => {
   const kakaoOnFailure = (error: any) => {
     console.log(error);
   };
+
+  // 이 부분에서 SWR을 사용하여 요청을 보내도록 수정
+  const { data: responseData, error } = useSWR(`/api/some-endpoint`, (url) => axios.get(url).then((res) => res.data));
+
+  useEffect(() => {
+    if (responseData) {
+      // 요청이 성공했을 때 처리할 로직
+      console.log('Response:', responseData);
+    }
+
+    if (error) {
+      // 요청이 실패했을 때 처리할 로직
+      console.log('Error:', error);
+    }
+  }, [responseData, error]);
 
   return (
     <>
