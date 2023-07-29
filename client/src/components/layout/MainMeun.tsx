6009949b1c0ee99486259ce0b1d177da8dc539ac
gwonyejun <kyjuni1059@gmail.com>
2023-07-29 17:11:46 +0900

    main meun메뉴바 영역 나누어 보기

--- client/src/components/layout/MainMeun.tsx
+++ client/src/components/layout/MainMeun.tsx
@@ -8,7 +8,9 @@ const MainMeun: React.FC = () => {
   return (
     <>
       <div className="mainMeun">
-
+        <div className='test1'>1</div>
+        <div className='test2'>2</div>
+        <div className='test3'>3</div>
         meun
       </div>
     </>
