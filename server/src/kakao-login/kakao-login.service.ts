1f160953aa0cb4192b0003ba8d1960bc82095be5
gwonyejun <kyjuni1059@gmail.com>
2023-08-03 17:51:49 +0900

    데이터 확인 로직 추가

--- server/src/kakao-login/kakao-login.service.ts
+++ server/src/kakao-login/kakao-login.service.ts
@@ -42,30 +42,35 @@ export class KakaoLoginService {
     ) {}
 
     async getUserInfo(code: string) {
+      console.log('service',code)
       const client_id = this.configService.get<string>('ab73463344398ef2f9f175e4c8801d20');
       const redirect_url = this.configService.get<string>('http://localhost:2222/');
+
+      // const redirect_url = 'http://localhost:2222';
+
       const tokenRequestUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${client_id}&redirect_uri=${redirect_url}&code=${code}`;
-  
+
       const tokenResponse = await this.httpService.get(tokenRequestUrl).toPromise();
+  console.log('data',tokenResponse);
       
       // const tokenResponse = await this.httpService
       // .post(tokenRequestUrl, {}, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } })
       // .toPromise();
-      const accessToken = tokenResponse.data.access_token;
+      // const accessToken = tokenResponse.data.access_token;
 
-      const userProfileUrl = 'https://kapi.kakao.com/v2/user/me';
-      const { data } = await this.httpService
-        .get(userProfileUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        })
-        .toPromise();
-  
+      // const userProfileUrl = 'https://kapi.kakao.com/v2/user/me';
+      // const { data } = await this.httpService
+      //   .get(userProfileUrl, {
+      //     headers: {
+      //       Authorization: `Bearer ${accessToken}`,
+      //     },
+      //   })
+      //   .toPromise();
+  // console.log('data',data);
       // 데이터 처리 및 저장 작업 수행
       // ...
       
-      return data;
+      return code;
     }
   }
 
