5e3639263c5b20763a3241e2b08d0148a883e727
gwonyejun <kyjuni1059@gmail.com>
2023-08-05 13:34:00 +0900

    로그인 모달창 버전 연구

--- client/src/components/service/KakaoLogin.tsx
+++ client/src/components/service/KakaoLogin.tsx
@@ -1,10 +1,61 @@
-import React, { useEffect } from 'react';
+// import React, { useEffect } from 'react';
+// import useSWR from 'swr';
+
+// // 카카오 앱 키
+// const meKey = 'ab73463344398ef2f9f175e4c8801d20';
+// // 카카오 등록 url
+// const Redirect_URL = 'http://localhost:2222/';
+// const urlParams = new URLSearchParams(window.location.search);
+// const authorizationCode = urlParams.get('code');
+
+// const fetcher = async (url: any) => {
+//   const response = await fetch(url);
+//   if (!response.ok) {
+//     throw new Error('Failed to fetch data');
+//   }
+//   return response.json();
+// };
+
+// const KakaoLoginComponent: React.FC = () => {
+//   // useSWR 훅 호출을 조건부가 아닌 항상 일정하게 호출되게 수정
+//   const { data, error } = useSWR(
+//     authorizationCode ? `/kakao-login/userinfo?code=${authorizationCode}` : null,
+//     fetcher
+//   );
+
+//   const handleKakaoLogin = async () => {
+//     const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${meKey}&redirect_uri=${Redirect_URL}&response_type=code`;
+//     window.location.assign(kakaoAuthURL);
+//   };
+
+//   // 로그인 처리 및 에러 처리
+//   useEffect(() => {
+//     if (data) {
+//       // 로그인 처리 예
+//       localStorage.setItem('token', data.token);
+//       // 사용자 데이터 저장
+//       // ...
+//     } else if (error) {
+//       console.error(error);
+//     }
+//   }, [data, error]);
+
+//   return (
+//     <div>
+//       <button onClick={handleKakaoLogin}>Kakao Login</button>
+//     </div>
+//   );
+// };
+
+// export default KakaoLoginComponent;
+
+import React, { useState, useEffect } from 'react';
 import useSWR from 'swr';
 
-// 카카오 앱 키
 const meKey = 'ab73463344398ef2f9f175e4c8801d20';
-// 카카오 등록 url
 const Redirect_URL = 'http://localhost:2222/';
+
+
 const urlParams = new URLSearchParams(window.location.search);
 const authorizationCode = urlParams.get('code');
 
@@ -16,29 +67,34 @@ const fetcher = async (url: any) => {
   return response.json();
 };
 
+
 const KakaoLoginComponent: React.FC = () => {
+  const [popup, setPopup] = useState<Window | null>(null);
   // useSWR 훅 호출을 조건부가 아닌 항상 일정하게 호출되게 수정
   const { data, error } = useSWR(
     authorizationCode ? `/kakao-login/userinfo?code=${authorizationCode}` : null,
     fetcher
   );
-
-  const handleKakaoLogin = async () => {
+  const handleKakaoLogin = () => {
     const kakaoAuthURL = `https://kauth.kakao.com/oauth/authorize?client_id=${meKey}&redirect_uri=${Redirect_URL}&response_type=code`;
-    window.location.assign(kakaoAuthURL);
+    const newPopup = window.open(kakaoAuthURL, 'Kakao Login', 'width=500,height=600');
+    setPopup(newPopup);
   };
 
-  // 로그인 처리 및 에러 처리
-  useEffect(() => {
-    if (data) {
-      // 로그인 처리 예
-      localStorage.setItem('token', data.token);
-      // 사용자 데이터 저장
-      // ...
-    } else if (error) {
-      console.error(error);
+  const checkPopup = () => {
+    if (!popup || popup.closed) {
+      setPopup(null);
     }
-  }, [data, error]);
+  };
+
+
+
+  useEffect(() => {
+    window.addEventListener('focus', checkPopup);
+    return () => {
+      window.removeEventListener('focus', checkPopup);
+    };
+  }, []);
 
   return (
     <div>
