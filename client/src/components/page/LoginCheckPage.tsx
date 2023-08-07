373b17c5dfbca502c54b1a0504929d05e97f7d5e
gwonyejun <kyjuni1059@gmail.com>
2023-08-07 20:08:57 +0900

    로그인 체크 페이지 구상

---
+++ client/src/components/page/LoginCheckPage.tsx
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from 'react';
+
+import Main from '../layout/Main'
+import Header from '../layout/Header'
+
+const LoginCheckPage: React.FC = () => {
+
+
+  return (
+    <>
+  
+    </>
+
+  )
+
+}
+
+
+
+export default LoginCheckPage
