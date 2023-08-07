7d980d539179641582da5ed2849c14b724f51a50
gwonyejun <kyjuni1059@gmail.com>
2023-08-07 20:13:58 +0900

    라우팅 준비

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -11,10 +11,9 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-  <BrowserRouter>
-        <App />
-  </BrowserRouter>
-  
-</React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
 );
 
