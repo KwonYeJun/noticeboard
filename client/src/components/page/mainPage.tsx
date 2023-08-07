52b8e526929bbfb241f618961cdd483227997b84
gwonyejun <kyjuni1059@gmail.com>
2023-08-07 20:04:06 +0900

    메인 페이지겸 첫 페이지를 보여주는 컴포넌트 생성

---
+++ client/src/components/page/mainPage.tsx
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+import Main from '../layout/Main'
+import Header from '../layout/Header'
+
+const MainPage: React.FC = () => {
+
+
+  return (
+    <>
+      <Header />
+      <Main />
+    </>
+
+  )
+
+}
+
+
+
+export default MainPage
