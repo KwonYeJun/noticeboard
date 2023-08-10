2e8fce0b6326d01d655e69e484a9326a45d82d86
gwonyejun <kyjuni1059@gmail.com>
2023-08-10 18:57:53 +0900

    meun영역 다시 불러오기

--- client/src/components/layout/MainMeun.tsx
+++ client/src/components/layout/MainMeun.tsx
@@ -21,9 +21,77 @@ const MainMeun: React.FC<MainMeunProps> = ({ handleNextSlide, handlePrevSlide })
       }
     }
   };
+  const list = {
+    hidden: {
+      opacity: 0
+    },
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3
+      }
+    }
+  };
+  // li자식 아이템 효과를 줄 수 있는 영역이다.
+  const item = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { rotate: 360, opacity: 1, y: 0 },
 
+
+  };
+  const itemStyle = {
+    borderLeft: 'none',
+    borderRight: 'none',
+    borderTop: 'none',
+  }
   return (
     <div className="mainMeun">
+      <motion.div variants={list} initial="hidden" animate="visible"
+          className='mainMeunBox'
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            width: '70%',
+            height: '35%',
+            borderRadius: 20,
+            listStyleType: 'none',
+          }}>
+          1
+        </motion.div>
+
+        <motion.div variants={list} initial="hidden" animate="visible"
+          className='mainMeunBox'
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            width: '70%',
+            height: '20%',
+            borderRadius: 20,
+            listStyleType: 'none',
+          }}>
+          1
+        </motion.div>
+        <motion.div variants={list} initial="hidden" animate="visible"
+          className='mainMeunBox'
+          style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'space-around',
+            width: '70%',
+            height: '20%',
+            borderRadius: 20,
+            listStyleType: 'none',
+          }}>
+          1
+        </motion.div>
+      
+      
       <motion.div
         variants={listVariants}
         initial="hidden"
