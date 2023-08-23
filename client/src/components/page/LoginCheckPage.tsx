5d3053626f63c0e18c23ab4f923d62013670bff4
gwonyejun <kyjuni1059@gmail.com>
2023-08-23 16:12:29 +0900

    form양식 생성 및 크기 정하기

--- client/src/components/page/LoginCheckPage.tsx
+++ client/src/components/page/LoginCheckPage.tsx
@@ -1,17 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState,useRef } from 'react';
 import KakaoLoginComponent from '../service/KakaoLogin'
 import '../css/loginPage.css';
+import lottie from 'lottie-web'; 
+import logoAnimation from '../lottiefile/textLogo.json';
 
 const LoginCheckPage: React.FC = () => {
+
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const animationRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (animationRef.current) {
+      const anim = lottie.loadAnimation({
+        container: animationRef.current,
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        animationData: logoAnimation
+      });
+
+      return () => {
+        anim.destroy(); // Cleanup the animation when the component unmounts
+      };
+    }
+  }, []);
+
+  const handleUsernameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setPassword(event.target.value);
+  };
+
+  const handleLogin = () => {
+    // 로그인 처리 로직을 여기에 구현
+    console.log('Username:', username);
+    console.log('Password:', password);
+  };
 
 
   return (
     <>
-      {/* <KakaoLoginComponent /> */}
-      500 , 652  
+      {/* <KakaoLoginComponent /> */} 
       <div className="loginLayout">
-
-      <img src='./test.png' alt="asd" className='textLogo' />
+      <img src="./test.png" alt="asd" className='textLogo' />
+      <div className="login-form">
+      <div className="input-container">
+        <input
+          type="text"
+          placeholder="Username"
+          value={username}
+          onChange={handleUsernameChange}
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <div className="button-container">
+        <div ref={animationRef} style={{ width: '100px', height: '100px' }} />
+        <button onClick={handleLogin}>Login</button>
+      </div>
+    </div>
       </div>
 
     </>
