17b905c3c154d378364f52b36ddbf0425720b84c
gwonyejun <kyjuni1059@gmail.com>
2023-08-23 15:32:52 +0900

    css 파일 추가 완료

--- client/src/components/page/LoginCheckPage.tsx
+++ client/src/components/page/LoginCheckPage.tsx
@@ -1,13 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import KakaoLoginComponent from '../service/KakaoLogin'
-
+import '../css/loginPage.css';
 
 const LoginCheckPage: React.FC = () => {
 
 
   return (
     <>
-      <KakaoLoginComponent />
+      {/* <KakaoLoginComponent /> */}
+        
     </>
 
   )
