c6d5b16d240c7f3bd4ff70d08f9a6ddff5dadef6
gwonyejun <kyjuni1059@gmail.com>
2023-08-24 20:46:37 +0900

    axios로 요청을 한다.

---
+++ client/src/components/user-auth-login/path-to-login.ts
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const API_BASE_URL = 'http://your-server-api-url';
+
+// 로그인 요청을 보내는 함수
+export const loginUser = async (username: string, password: string) => {
+  try {
+    const response = await axios.post(`${API_BASE_URL}/login`, { username, password });
+    return response.data;
+  } catch (error) {
+    throw error;
+  }
+};
